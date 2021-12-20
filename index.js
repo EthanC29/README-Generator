d706348d6b94a26f7613cf32cec70dcb099b6c04
Ethan <90936828+EthanC29@users.noreply.github.com>
2021-12-19 21:15:37 -0500

    working

--- index.js
+++ index.js
@@ -1,130 +1,126 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("/utils/generateMarkdown.js")
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Generate an array of questions
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your application? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description for your application (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions for your application'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage instructions for your application'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Provide contribution guidelines for your application'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Provide ways to test your application'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license (Required)',
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ],
+            validate: licenseInput => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license');
+                    return false;
+                }
+            }
+            
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email');
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    const generatePage = (title, dataObj) => {
+function writeToFile(data) {
 
-        let description = dataObj.description;
-        let installation = dataObj.installation;
-        let usage = dataObj.usage;
-        let license = dataObj.license;
-        let licenseBadge = dataObj.licenseBadge;
-        let contribution = dataObj.contribution;
-        let testInstructions = dataObj.testInstructions;
-        let usernameGitHub = dataObj.username;
-        let email = dataObj.email;
-
-        return `
-        # ${title}
-        ${licenseBadge}
-
-        ## Description
-        ${description}
-
-        ## Table of Contents
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Contributing](#contributing)
-        * [Tests](#tests)
-        * [Questions](#questions)
-
-        ## Installation
-        ${installation}
-
-        ## Usage
-        ${usage}
-        
-        ## License
-        This application is covered under ${license}
-        
-        ## Contributing
-        ${contribution}
-        
-        ## Tests
-        ${testInstructions}
-        
-        ## Questions
-        Created by: [${usernameGitHub}](https://github.com/${usernameGitHub})
-        For any additional questions, I can be reached at [${email}](mailto:${email})
-        `;
-
-    };
-
-    fs.writeFile('README.md', generatePage(fileName, data), err => {
+    fs.writeFile('README.md', generateMarkdown(data), err => {
         if (err) throw err;
         console.log('README complete!');
     });
+
 }
 
 // TODO: Create a function to initialize app
 function init() {
 
-    const promptUser = () => {
-        return inquirer.prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: 'What is the name of your application? (Required)'
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Provide a description for your application (Required)'
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'Provide installation instructions for your application'
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'Provide usage instructions for your application'
-            },
-            {
-                type: 'input',
-                name: 'contribution',
-                message: 'Provide contribution guidelines for your application'
-            },
-            {
-                type: 'input',
-                name: 'test',
-                message: 'Provide ways to test your application'
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: 'Choose a license (Required)',
-                choices: 'Apache, Boost, BSD, Creative Commons, Eclipse, GNU, The Organization for Ethical Source, IBM, ISC, MIT, Mozilla, Open Data Commons, Perl, SIL, Unlicense, WTFPL, Zlib',
-                default: 'Apache'
-            },
-            {
-                type: 'input',
-                name: 'github-username',
-                message: 'What is your GitHub username? (Required)'
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'What is your email address? (Required)'
-            }
-        ]);
-    };
     
-    promptUser().then(answers => writeToFile(answers.title, answers));
-
-
-
-
-
     
+    promptUser().then(answers => writeToFile(answers));
+
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  
-}
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(dataObj) {
 
-`;
+  return `
+  # ${dataObj.title}
+  ![License Badge](https://img.shields.io/badge/license-${dataObj.license}-brightgreen)
+
+  ## Description
+  ${dataObj.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${dataObj.installation}
+
+  ## Usage
+  ${dataObj.usage}
+  
+  ## License
+  ![License Badge](https://img.shields.io/badge/license-${dataObj.license}-brightgreen)
+  This application is covered by ${dataObj.license} license.
+  
+  ## Contributing
+  ${dataObj.contribution}
+  
+  ## Tests
+  ${dataObj.test}
+  
+  ## Questions
+  Created by: [${dataObj.username}](https://github.com/${dataObj.username})
+  For any additional questions, I can be reached at [${dataObj.email}](mailto:${dataObj.email})
+  `;
+
 }
 
 module.exports = generateMarkdown;
